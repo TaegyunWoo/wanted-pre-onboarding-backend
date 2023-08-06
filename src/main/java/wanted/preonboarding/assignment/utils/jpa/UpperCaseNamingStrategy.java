3e2f34a8debc95eb0eeccf215ab39c218ba5f6ea
TaegyunWoo <dnxprbs@gmail.com>
2023-08-06 15:27:08 +0900

    [Setting] JPA DDL AUTO Naming Strategy 설정
    JPA의 DDL-AUTO를 통해 생성되는 테이블·칼럼의 이름에 대문자·언더바 전략을 적용

---
+++ src/main/java/wanted/preonboarding/assignment/utils/jpa/UpperCaseNamingStrategy.java
@@ -0,0 +1,22 @@
+/**
+ * packageName    : wanted.preonboarding.assignment.util.jpa
+ * fileName       : UpperCaseNamingStrategy
+ * author         : 우태균
+ * date           : 2023/08/06
+ * description    : JPA Configuration 에 DDL AUTO 설정시, 네이밍 전략
+ *                  모두 대문자 + 언더바 조합으로 생성함
+ */
+package wanted.preonboarding.assignment.utils.jpa;
+
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+import java.util.Locale;
+
+public class UpperCaseNamingStrategy extends CamelCaseToUnderscoresNamingStrategy {
+  @Override
+  protected Identifier getIdentifier(String name, boolean quoted, JdbcEnvironment jdbcEnvironment) {
+    return new Identifier(name.toUpperCase(Locale.ROOT), quoted);
+  }
+}
