5c1d091efeeca358abd5bbcdfedecbe4a8092314
TaegyunWoo <dnxprbs@gmail.com>
2023-08-06 22:23:41 +0900

    [Feature] Add All Entities

---
+++ src/main/java/wanted/preonboarding/assignment/domain/Post.java
@@ -0,0 +1,35 @@
+/**
+ * packageName    : wanted.preonboarding.assignment.domain
+ * fileName       : Post
+ * author         : 우태균
+ * date           : 2023/08/06
+ * description    : 게시글 엔티티
+ */
+package wanted.preonboarding.assignment.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "POST")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@SuperBuilder
+public class Post extends BaseTimeEntity {
+  @Id @GeneratedValue
+  private Long id;
+  @Column(length = 255, nullable = false)
+  private String title;
+  @Lob
+  @Column(nullable = false)
+  private String body;
+  @Column(nullable = false)
+  private Integer views;
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  private User author;
+}
---
+++ src/main/java/wanted/preonboarding/assignment/domain/User.java
@@ -0,0 +1,38 @@
+/**
+ * packageName    : wanted.preonboarding.assignment.domain
+ * fileName       : User
+ * author         : 우태균
+ * date           : 2023/08/06
+ * description    : 사용자 엔티티
+ */
+package wanted.preonboarding.assignment.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "USER")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@SuperBuilder
+public class User extends BaseTimeEntity {
+  @Id @GeneratedValue
+  private Long id;
+  @Column(nullable = false, unique = true)
+  private String accountId;
+  @Column(nullable = false)
+  private String password;
+
+  //양방향 설정
+  @Builder.Default
+  @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  private List<Post> postList = new ArrayList<>();
+}
