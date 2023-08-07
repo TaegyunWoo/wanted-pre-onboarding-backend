9e87c5a35054ce0f94b41edf43ef372fb2ef87b3
TaegyunWoo <dnxprbs@gmail.com>
2023-08-07 23:05:28 +0900

    [Setting] Change Redis Template Serialize Type to String
    Redis CLI를 통해, 직접 확인할 때도 문자열 형식으로 알아볼 수 있게끔 String 형으로 직렬화 수행하도록 수정함.

--- src/main/java/wanted/preonboarding/assignment/config/RedisRepositoryConfig.java
+++ src/main/java/wanted/preonboarding/assignment/config/RedisRepositoryConfig.java
@@ -14,6 +14,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableRedisRepositories
 @Configuration
@@ -39,8 +40,10 @@ public class RedisRepositoryConfig {
    */
   @Bean
   public RedisTemplate<?, ?> redisTemplate() {
-    RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
     return redisTemplate;
   }
 }
