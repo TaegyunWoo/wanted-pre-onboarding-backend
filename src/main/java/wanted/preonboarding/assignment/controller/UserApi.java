8da5bc05c5483b44c3fdd3815b91528a577d4f25
TaegyunWoo <dnxprbs@gmail.com>
2023-08-07 16:59:01 +0900

    [Fix] User Api 관련 검증 애너테이션 교체
    input 값이 검증에 실패하여 Binding Exception을 바로 던지기 위해 수정함.
    만약 @Validated 사용시, 직접 오류를 던져야함.

--- src/main/java/wanted/preonboarding/assignment/controller/UserApi.java
+++ src/main/java/wanted/preonboarding/assignment/controller/UserApi.java
@@ -1,15 +1,15 @@
 package wanted.preonboarding.assignment.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import wanted.preonboarding.assignment.dto.TokenDto;
 import wanted.preonboarding.assignment.dto.UserDto;
+
+import javax.validation.Valid;
 
 @Tag(name = "사용자 API")
 @RequestMapping("/user")
@@ -18,13 +18,13 @@ public interface UserApi {
   @ApiResponse(responseCode = "200", description = "회원가입 성공")
   @PostMapping
   void postSignUp(
-      @RequestBody @Validated UserDto.UserRequest userRequest
+      @RequestBody @Valid UserDto.UserRequest userRequest
   );
 
   @Operation(summary = "로그인")
   @ApiResponse(responseCode = "200", description = "로그인 성공")
   @PostMapping("/sign-in")
   TokenDto.TokenResponse postSignIn(
-      @RequestBody @Validated UserDto.UserRequest userRequest
+      @RequestBody @Valid UserDto.UserRequest userRequest
   );
 }
--- src/main/java/wanted/preonboarding/assignment/controller/UserController.java
+++ src/main/java/wanted/preonboarding/assignment/controller/UserController.java
@@ -1,14 +1,19 @@
 package wanted.preonboarding.assignment.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import wanted.preonboarding.assignment.service.UserService;
 
 import static wanted.preonboarding.assignment.dto.TokenDto.TokenResponse;
 import static wanted.preonboarding.assignment.dto.UserDto.UserRequest;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
 public class UserController implements UserApi {
+  private final UserService userService;
+
   @Override
   public void postSignUp(UserRequest userRequest) {
 
